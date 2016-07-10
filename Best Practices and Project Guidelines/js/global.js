5f304e52553e265697de0713823d53a1bdc6bab1
Navendu Sanwal <navendu.bsf@gmail.com>
2016-07-10 20:06:22 +0530

    Adding HTML template for code snippets and css

--- Best Practices and Project Guidelines/js/global.js
+++ Best Practices and Project Guidelines/js/global.js
@@ -1,7 +1,6 @@
-
 /*
  * Keep Table of Contents position fixed on scroll
-*/
+ */
 window.addEventListener('scroll', function() {
 	if (window.scrollY > 150 || window.pageYOffset > 150) {
 		document.getElementsByTagName('nav')[0].style.position = 'fixed';
@@ -9,3 +8,28 @@ window.addEventListener('scroll', function() {
 		document.getElementsByTagName('nav')[0].style.position = '';
 	}
 });
+
+/*
+ * Template for code examples
+ */
+var template = document.getElementById('code-example'),
+	holder = document.querySelectorAll('.holder'),
+	clone, badCodeSrc, goodCodeSrc, badTitle, goodTitle,
+	i = 0,
+	l = holder.length,
+	tabSpace = '	';
+
+for (; i < l; i++) {
+	clone = template.content.cloneNode(true);
+
+	badCodeSrc = holder[i].querySelector('code.bad');
+	goodCodeSrc = holder[i].querySelector('code.good');
+
+	clone.querySelector('.bad .title').innerHTML  = badCodeSrc.title || 'Bad';
+	clone.querySelector('.good .title').innerHTML = goodCodeSrc.title || 'Good';
+	clone.querySelector('.bad code').innerHTML    = badCodeSrc.innerHTML;
+	clone.querySelector('.good code').innerHTML   = goodCodeSrc.innerHTML;
+
+	holder[i].innerHTML = '';
+	holder[i].appendChild(clone);
+}
